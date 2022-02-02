1a5eb8a5f2629c8970e751936e84fb83dd8f287f
sudhanshu2710 <sudhanshu.singh2710@gmail.com>
2022-02-02 11:23:11 +0530

    second

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "./index.css";
 const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
 
-const html = `<div class="main">
-<h1 class="head">This is weppack</h1>
-<p>this is use to bunddel the code in our project</p>
-</div>`;
-
-container.insertAdjacentHTML("beforeend", html);
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const note = document.querySelector(".note").value;
+  const html = `
+    <h1 class="head">${note}</h1>
+    `;
+  container.insertAdjacentHTML("beforeend", html);
+});
